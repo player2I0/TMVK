b62f8cde2da3280288182ed046bbd4ef795e9ef0
player2I0 <59975613+player2I0@users.noreply.github.com>
2022-07-20 23:39:45 +0200

    1.0

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1658353147,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/button_outline.png",
+		"images/button-sheet0.png",
+		"images/bgimg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
